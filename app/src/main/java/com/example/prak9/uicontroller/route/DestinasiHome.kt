74c1939322d7cb5df261b5444b7cadd734a59c38
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-12-26 20:13:44 +0700

    Mengganti judul halaman utama dari "nama" menjadi nama aplikasi

--- app/src/main/java/com/example/prak9/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/prak9/uicontroller/route/DestinasiHome.kt
@@ -2,7 +2,7 @@ package com.example.prak9.uicontroller.route
 
 import com.example.prak9.R
 
-object DestinasiHome: DestinasiNavigasi{
+object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
-    override val titleRes = R.string.nama
+    override val titleRes = R.string.app_name
 }
