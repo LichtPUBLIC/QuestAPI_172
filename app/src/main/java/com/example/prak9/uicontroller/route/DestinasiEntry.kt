d38dd7ae200bafcb3ffeb9f11e8dbe8d782116e8
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-12-26 20:13:21 +0700

    Menambahkan objek navigasi DestinasiEntry untuk halaman input data

--- app/src/main/java/com/example/prak9/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/prak9/uicontroller/route/DestinasiEntry.kt
@@ -2,7 +2,8 @@ package com.example.prak9.uicontroller.route
 
 import com.example.prak9.R
 
-object DestinasiEntry: DestinasiNavigasi{
+object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
+
 }
