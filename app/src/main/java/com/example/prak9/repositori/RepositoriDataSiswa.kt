edaf70741d55a64fbde5c06f513c654926f429f1
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-12-26 20:10:14 +0700

    Implementasi repository jaringan menghubungkan ke ServiceApiSiswa

--- app/src/main/java/com/example/prak9/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/prak9/repositori/RepositoriDataSiswa.kt
@@ -9,4 +9,19 @@ interface RepositoryDataSiswa {
     suspend fun getSatuSiswa(id:Int) : DataSiswa
     suspend fun editSatuSiswa (id:Int,datasiswa: DataSiswa) : retrofit2.Response<Void>
     suspend fun hapusSatuSiswa (id:Int) : retrofit2.Response<Void>
+
+}
+
+class jaringanRepositoryDataSiswa (
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
+    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
+
 }
