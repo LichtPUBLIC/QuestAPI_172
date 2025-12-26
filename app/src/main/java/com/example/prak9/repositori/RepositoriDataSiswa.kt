8ee5a6d07abcaa1db381728708b21af12d48340a
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-12-26 20:09:46 +0700

    Membuat interface RepositoryDataSiswa untuk definisi operasi CRUD

--- app/src/main/java/com/example/prak9/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/prak9/repositori/RepositoriDataSiswa.kt
@@ -1,18 +1,12 @@
 package com.example.prak9.repositori
 
-import com.example.prak9.apiservice.ServiceApiSiswa
 import com.example.prak9.modeldata.DataSiswa
-
+import com.example.prak9.apiservice.ServiceApiSiswa
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2.Response<Void>
-}
-
-class jaringanRepositoryDataSiswa (
-    private val serviceApiSiswa: ServiceApiSiswa
-): RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
-    .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa (id:Int,datasiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa (id:Int) : retrofit2.Response<Void>
 }
